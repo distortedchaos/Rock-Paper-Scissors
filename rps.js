8c6560f8822d3f319568af8dbd1ef1dc9491b618
distortedchaos <150536457+distortedchaos@users.noreply.github.com>
2024-01-18 14:13:02 +0530

    Update rps.js

--- rps.js
+++ rps.js
@@ -14,70 +14,69 @@ let yourscore = document.querySelector(".urscorenum");
 let comscore = document.querySelector(".compscorenum");
 let draw = document.querySelector(".btnn");
 rock.addEventListener("click", () => {
-    if (randnum == 1) {
-      draw.innerText = "It was a draw";
-      randumnumber();
-    } else if (randnum == 2) {
-      compscore++;
-      console.log("Comp=" + compscore);
-      comscore.innerText = compscore;
-      draw.innerText = "Paper beats Rock";
-      randumnumber();
-
-    }
-   else if (randnum == 3) {
+  if (randnum == 1) {
+    draw.innerText = "It was a draw";
+    draw.style.backgroundColor = "rgb(25, 51, 78)";
+    randumnumber();
+  } else if (randnum == 2) {
+    compscore++;
+    console.log("Comp=" + compscore);
+    comscore.innerText = compscore;
+    draw.innerText = "You loose.Paper beats Rock";
+    draw.style.backgroundColor = "red";
+    randumnumber();
+  } else if (randnum == 3) {
     urscore++;
     console.log("You=" + urscore);
     yourscore.innerText = urscore;
-    draw.innerText = "Rock beats Scissor";
+    draw.innerText = "You win.Rock beats Scissor";
+    draw.style.backgroundColor = "green";
     randumnumber();
-
-  }});
+  }
+});
 
 let paper = document.querySelector(".img2");
-paper.addEventListener(
-  "click",
-  () => {
-      if (randnum == 2) {
-        draw.innerText = "It was a draw";
-        randumnumber();
-
-      } else if (randnum == 3) {
-        compscore++;
-        console.log("Comp=" + compscore);
-        comscore.innerText = compscore;
-        draw.innerText = "Scissor beats Paper";
-        randumnumber();
- 
-      } else if (randnum == 1) {
-        urscore++;
-        console.log("You=" + urscore);
-        yourscore.innerText = urscore;
-        draw.innerText = "Paper beats Rock";
-        randumnumber();
-
-      }
-  });
+paper.addEventListener("click", () => {
+  if (randnum == 2) {
+    draw.innerText = "It was a draw";
+    draw.style.backgroundColor = "rgb(25, 51, 78)";
+    randumnumber();
+  } else if (randnum == 3) {
+    compscore++;
+    console.log("Comp=" + compscore);
+    comscore.innerText = compscore;
+    draw.innerText = "You loose.Scissor beats Paper";
+    draw.style.backgroundColor = "red";
+    randumnumber();
+  } else if (randnum == 1) {
+    urscore++;
+    console.log("You=" + urscore);
+    yourscore.innerText = urscore;
+    draw.innerText = "You win.Paper beats Rock";
+    draw.style.backgroundColor = "green";
+    randumnumber();
+  }
+});
 
 let scissor = document.querySelector(".img3");
 scissor.addEventListener("click", () => {
-    if (randnum == 3) {
-      draw.innerText = "It was a draw";
-      randumnumber();
- 
-    } else if (randnum == 1) {
-      compscore++;
-      console.log("Comp=" + compscore);
-      comscore.innerText = compscore;
-      draw.innerText = "Rock beats Scissor";
-      randumnumber();
-      
-    } else if (randnum == 2) {
-      urscore++;
-      console.log("You=" + urscore);
-      yourscore.innerText = urscore;
-      draw.innerText = "Scissor beats Paper";      
-      randumnumber();
-    }
+  if (randnum == 3) {
+    draw.innerText = "It was a draw";
+    draw.style.backgroundColor = "rgb(25, 51, 78)";
+    randumnumber();
+  } else if (randnum == 1) {
+    compscore++;
+    console.log("Comp=" + compscore);
+    comscore.innerText = compscore;
+    draw.innerText = "You loose.Rock beats Scissor";
+    draw.style.backgroundColor = "red";
+    randumnumber();
+  } else if (randnum == 2) {
+    urscore++;
+    console.log("You=" + urscore);
+    yourscore.innerText = urscore;
+    draw.innerText = "You win.Scissor beats Paper";
+    draw.style.backgroundColor = "green";
+    randumnumber();
+  }
 });
-
